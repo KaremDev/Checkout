7a5a7af9b21dae4c86e6150c2a665ae11e99b284
kmahmoud <kmahmoud@TMK-LAP-EGPT-13>
2021-08-18 16:46:57 +0200

    Add Checkout dll

---
+++ Checkout/Controllers/CheckoutController.cs
@@ -0,0 +1,37 @@
+﻿using Checkout.Common;
+using Checkout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Checkout.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = PrepareModel();
+            return View(model);
+        }
+
+        public ActionResult Post()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        private PaymentModel PrepareModel(PaymentModel existingModel = null)
+        {
+            var model = existingModel ?? new PaymentModel();
+            model.Currencies = new[]
+            {
+                new SelectListItem() {Value = Currency.USD, Text = Currency.USD},
+                new SelectListItem() {Value = Currency.EUR, Text = Currency.EUR},
+                new SelectListItem() {Value = Currency.GBP, Text = Currency.GBP}
+            };
+            return model;
+        }
+    }
+}
