43578c35a0f52466c47fce00e9ac4d4b680fbaee
Karim Abdel Samad <ksamad@tamkeenhr.com>
2021-08-19 09:46:53 +0200

    Added Api controller

---
+++ Checkout/Controllers/PayController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Checkout.Controllers
+{
+    public class PayController : ApiController
+    {
+        // GET: api/Pay
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Pay/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Pay
+        public void Post([FromBody]string value)
+        {
+        }
+
+        
+    }
+}
